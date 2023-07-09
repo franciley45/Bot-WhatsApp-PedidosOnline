1b905b1f155e1d0d003ccf38d701a79f9a56aa9e
franciley45 <testepa45@gmail.com>
2023-07-09 15:52:57 -0300

    installation is venom-bot initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("vamos lá")
+import { Message, Whatsapp, create } from "venom-bot"
+
+create({
+  session: "food-gpt",
+  disableWelcome: true,
+})
+  .then(async (client: Whatsapp) => await start(client))
+  .catch((err) => {
+    console.log(err)
+  })
+
+async function start(client: Whatsapp) {
+  client.onMessage(async (message: Message) => {
+    if (!message.body || message.isGroupMsg) return
+
+    const response = `Olá!`
+
+    await client.sendText(message.from, response)
+  })
+}
