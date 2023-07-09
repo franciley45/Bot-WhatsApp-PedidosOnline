affcc7e86cf9932c18fce95abb460d6d46e37cfe
franciley45 <testepa45@gmail.com>
2023-07-09 17:06:14 -0300

    function completion no arquivo index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { Message, Whatsapp, create } from "venom-bot"
+import { openai } from "./lib/openai"
+import { ChatCompletionRequestMessage } from "openai"
+
+async function completion(
+  messages: ChatCompletionRequestMessage[]
+): Promise<string | undefined> {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    max_tokens: 256,
+    messages,
+  })
+
+  return completion.data.choices[0].message?.content
+}
 
 create({
   session: "food-gpt",
